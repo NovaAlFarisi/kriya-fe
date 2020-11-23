a0a4782c32173f584277d0990daffac13425bb17
Nova Al Farisi <novaa.alfarisi@gmail.com>
2020-11-23 09:05:19 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react';
+import KriyaApi from './api/KriyaApi'
+import Product from './components/product/productComponent'
 function App() {
+  const [products, setProducts] = useState([])
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        await KriyaApi.get('/').then(result=>{
+          let data = result.data;
+          let productsData = data.map((item,i)=>{
+            return {...item, qty:0}
+          })
+          setProducts(productsData)
+        })
+      } catch (error) {
+        alert('Network error')
+      }
+    }
+
+    fetchData()
+  },[])
+  console.log(products)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Products</h1>
+      {(!products ? (<p>Fetching products ...</p>) : (
+        <div className="products-wrapper">
+          {products.map((val,i)=>(
+            <Product key={i} identifier={i} data={val}/>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }

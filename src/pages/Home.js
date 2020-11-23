c8b95de8b7d34e087cf1b3e3ad551946c7c4c65d
Nova Al Farisi <novaa.alfarisi@gmail.com>
2020-11-23 19:40:04 +0700

    removed unused vars

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
 import {useContext} from 'react'
-import {Link} from 'react-router-dom'
 import {ItemContext} from '../context/ItemContext';
-import {CartContext} from '../context/CartContext';
 import Product from '../components/product/ProductComponent';
 const Home = () => {
     const [item, setItem] = useContext(ItemContext);
-    const [cart, setCart] = useContext(CartContext);
     return (
         <div className="container">
         <h1>Products</h1>
